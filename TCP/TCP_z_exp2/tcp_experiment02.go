fbcf655521515418bff0ca867b53057aa25f6746
jwatson-CO-edu <james.watson-2@colorado.edu>
2021-01-12 12:28:34 -0700

    One struct to rule them all

--- TCP/TCP_z_exp2/tcp_experiment02.go
+++ TCP/TCP_z_exp2/tcp_experiment02.go
@@ -15,6 +15,7 @@ Template Ver. 2020-12
 /*** Imports ***/
 import (
 	"fmt" //- Printing
+	"net"
 	//"os" // Terminal args
 )
 
@@ -25,17 +26,42 @@ var fprintf = fmt.Printf //- Alias for PrintFormat
 
 /********** Structs ******************************************************************************/
 
+/***** NetAgent *****/
+type NetAgent struct {
+	/* Connection */
+	conn /*-*/ *net.Conn // Network interface
+	addrPtr    *net.Addr // Address object
+	host /*-*/ string // -- IP Address
+	port /*-*/ string // -- Port number
+	vers /*-*/ string // -- Protocol version
+	cnnctd     bool // ---- Is connected?
+	maxAttempt int // ----- Max number of attempts without giving up
+	/* TX/RX */
+	sendData []byte //- Data to send
+	recvData []byte //- Data received
+	netCode  int // --- Error code   from network ops
+	err /**/ error // - Error object from network ops
+	/* Timing */
+	duration_s float64 // How long to keep the connection alive in seconds
+	rateHz     float64 // How often to send/recv in Hz
+}
+
+func default_connection_spec() NetAgent {
+	// Return a `NetAgent` pre-populated with default connection data
+	return NetAgent{
+		host: "127.0.0.1",
+		port: "8000",
+		vers: "tcp6",
+	}
+}
+
 /***** Server *****/
 
-type Server struct {
 
-}
 
 /***** Client *****/
 
-type Client struct {
 
-}
 
 
 /********** MAIN *********************************************************************************/
